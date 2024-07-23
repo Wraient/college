65a85de6517c6676d5fef3a61d137d5a24e76a72
wraient <wraient@.com>
2024-07-23 13:20:28 +0530

    diffrent inputs

--- fds/assignment3.py
+++ fds/assignment3.py
@@ -1,4 +1,6 @@
-string = input("Enter the string: ")
+
+# Longest Word
+string = input("Enter the string to find longest word: ")
 
 string_list = string.split()
 longest = 0
@@ -8,8 +10,9 @@ for i in range(len(string_list)):
         longest = len(string_list[i])  
         index = i
 
-# print(f"Longest word in string: {max(string.split())}")
 print(f"Longest word in the string is: {string_list[index]}")
+
+# Number of occurence of character
 
 print("Enter the character you want to count the occurence of: ")
 char = input()[0]
@@ -20,12 +23,18 @@ for i in string:
 
 print(f"Occurence of char {char} in string is: {count} times")
 
+# Palindrome Check
+
 string = input("Enter the string: (for palindrome check) ")
 
 if string[::-1] == string:
     print("String is a palindrome")
 else:
     print("String is not a palindrome")
+
+# Subtring finding
+
+string = input("Enter a sentence: ")
 
 print("Enter the substring you want to search for: ")
 sub_string = input()
@@ -35,6 +44,8 @@ if index==-1:
     print("No substring found")
 else:
     print(f"Index of substring in string is at: {string.find(sub_string)}")
+
+# Number of occurence of word finding
 
 words = []
 occurence = []
